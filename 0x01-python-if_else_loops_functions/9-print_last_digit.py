81076feb51e86c0b8ca5f4903b0d1c894b487412
Hafsawane <aludra000@gmail.com>
2023-12-24 12:34:21 -0800

    python

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def print_last_digit(number):
-lastdigit = abs(number) % 10
-    if number < 0:
-                lastdigit = - lastdigit
-                        return lastdigit
-                        else:
-                                    return lastdigit
