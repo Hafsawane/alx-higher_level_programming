e9092f4dd9ad58514ec0f11c7bcf1a5c3b51215e
Hafsawane <aludra000@gmail.com>
2023-12-25 10:22:01 -0800

    python

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-for char in range(ord('Z'), ord('A'), -1):
+for char in range(ord('Z'), ord('A'), -1, -1):
     letter = chr(char)
     if char % 2 == 0:
         letter = letter.upper()
     else:
         letter = letter.lower()
-        print("{}".format(letter), end="")
+    print("{}".format(letter), end="")
 print()
