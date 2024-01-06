5dbd8d5080ba81c085c7afe3de20a226b726a921
Hafsawane <aludra000@gmail.com>
2024-01-05 16:32:48 -0800

    python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     else:
         print("{} arguments :".format(number))
 
-    for i in number:
+    for i in range(number):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
